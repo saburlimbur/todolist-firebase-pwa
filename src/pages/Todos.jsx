dd37b4b8606c4dd964a8cbeb00784960936f7b30
Alif Dwi Rahman <alifdwirahman54@gmail.com>
2025-01-28 00:58:38 +0700

    feat: fetched todos

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -8,7 +8,19 @@ function Todos() {
 
   return (
     <div>
-      <h2>Todos</h2>
+      {Array.isArray(allTodoQuery) &&
+        allTodoQuery.map((e) => {
+          const { title, status, description } = e;
+
+          return (
+            <div key={e.id}>
+              <p>title: {title}</p>
+              <p>status: {status.complete ? 'Complete' : status.pending ? 'Pending' : 'Archived'}</p>
+              <p>desc: {description}</p>
+              {/* <p>date: {created_at}</p> */}
+            </div>
+          );
+        })}
     </div>
   );
 }
